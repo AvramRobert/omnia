8d472d8e3af8a230c35eb6118e050d7e4dc2aebe
Robert Marius Avram <am11.robert@yahoo.com>
2017-02-25 23:07:42 +0100

    Project init

---
+++ src/omnia/core.clj
@@ -0,0 +1,7 @@
+(ns omnia.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/omnia/core_test.clj
@@ -0,0 +1,7 @@
+(ns omnia.core-test
+  (:require [clojure.test :refer :all]
+            [omnia.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
