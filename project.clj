48839ef5efec3b3ca75c5f57c6a7610fd6f80d6b
Robert Marius Avram <am11.robert@yahoo.com>
2017-03-01 23:27:58 +0100

    First base

--- project.clj
+++ project.clj
@@ -2,8 +2,10 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clojure-lanterna "0.9.7"]
+                 [org.clojure/core.match "0.3.0-alpha4"]]
   :main ^:skip-aot omnia.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
