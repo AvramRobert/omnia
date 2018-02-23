89630f3a8658c0bc1c5014274d92bdeb450eaf81
Robert M. Avram <robert.avram@rewe-digital.com>
2018-02-23 07:33:19 +0100

    V020: Also add license to project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject omnia "0.2.0"
   :description "A Clojure REPL for prototyping and experimenting"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/AvramRobert/omnia"
+  :license {:name "Apache-2.0 License"
+            :url  "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clojure-lanterna "0.10.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
