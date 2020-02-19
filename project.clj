1d6d470c2635bd1af8142961e8c8bcabe094ec5c
Robert Avram <am11.robert@yahoo.com>
2020-02-19 14:33:02 +0100

    Fix dependecies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/AvramRobert/omnia"
   :license {:name "Apache-2.0 License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [com.googlecode.lanterna/lanterna "3.0.1"]
                  [org.clojure/core.match "0.3.0-alpha5"]
-                 [fipp "0.6.12"]
+                 [fipp "0.6.22"]
                  [org.clojure/tools.nrepl "0.2.13"]
                  [halfling "1.1.1"]
                  [instaparse "1.4.9"]
