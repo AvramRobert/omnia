d1b64470aab5bf7f1a7d0600ed11910ead39d0da
Robert M. Avram <am11.robert@yahoo.com>
2021-04-20 01:39:10 +0200

    Split rendering lines from rendering highlights. Use direct indexes

--- src/omnia/view/render.clj
+++ src/omnia/view/render.clj
@@ -5,7 +5,7 @@
             [omnia.repl.hud :as h]
             [omnia.repl.context :as c]
             [omnia.config.components.core :refer [UserHighlighting]]
-            [omnia.config.components.text :refer [backgrounds coloured-element Style]]
+            [omnia.config.components.text :refer [backgrounds coloured-element Style ColouredElement]]
             [omnia.text.highlighting :refer [fold -text]]
             [omnia.repl.context :refer [Context]]
             [omnia.util.schema :refer [Point Region]]
@@ -22,13 +22,23 @@
    :closed-paren 2
    :open-paren   1})
 
+;(s/def PrintInstruction
+;  {:line                        [Character]
+;   :at                          s/Int
+;   :terminal                    t/Terminal
+;   :scheme                      UserHighlighting
+;   :styles                      [Style]
+;   (s/optional-key :sub-region) Point
+;   (s/optional-key :padding)    s/Int})
+
 (s/def PrintInstruction
   {:line                        [Character]
    :at                          s/Int
+   :start                       s/Int
+   :end                         s/Int
    :terminal                    t/Terminal
    :scheme                      UserHighlighting
    :styles                      [Style]
-   (s/optional-key :sub-region) Point
    (s/optional-key :padding)    s/Int})
 
 (s/defn additive-diff :- (s/maybe c/Highlight)
@@ -63,92 +73,162 @@
           (and exact-end? grown-top?)) (assoc current :region {:start [xs  ys] :end [xs' ys']})
       :else current)))
 
-(s/defn print-line!
-  [{line     :line
-    y        :at
-    padding  :padding
-    terminal :terminal
-    [xs xe]  :sub-region
-    cs       :scheme
-    styles   :styles} :- PrintInstruction]
-  (letfn [(put! [ch x y emission]
-            (let [fg (get cs (coloured-element emission))
-                  bg (get cs backgrounds)]
-              (t/put! terminal ch x y fg bg styles)))
-          (pad! [x]
-            (when padding
-              (dotimes [offset padding]
-                (put! \space (+ x offset) y -text))))
-          (print-from! [x emission chars]
-            (reduce-idx
-              (fn [x' _ input]
-                (put! input x' y emission)) x nil chars))
-          (print! [x emission chars]
-            (print-from! x emission chars)
-            (+ x (count chars)))
-          (print-sub! [x emission chars]
-            (let [x' (+ x (count chars))]
-              (cond
-                (and (<= x xs) (>= x' xe)) (->> chars
-                                                (drop (- xs x)) ;; subvecs
-                                                (take (- xe xs)) ;; subvecs
-                                                (print-from! xs emission))
-                (and (<= x xs) (> x' xs))  (->> chars
-                                                (drop (- xs x)) ;; subvecs
-                                                (print-from! xs emission))
-                (>= x' xe)                 (->> chars
-                                                (take (- xe x)) ;; subvecs
-                                                (print-from! x emission))
-                (> x' xs)                  (->> chars
-                                                (print-from! x emission))
-                :else                      nil)
-              x'))]
-    (-> (if (and xs xe) print-sub! print!)
-        (fold 0 line)
-        (pad!))))
+;(s/defn print-line!
+;  [{line     :line
+;    y        :at
+;    padding  :padding
+;    terminal :terminal
+;    [xs xe]  :sub-region
+;    cs       :scheme
+;    styles   :styles} :- PrintInstruction]
+;  (letfn [(put! [ch x y emission]
+;            (let [fg (get cs (coloured-element emission))
+;                  bg (get cs backgrounds)]
+;              (t/put! terminal ch x y fg bg styles)))
+;          (pad! [x]
+;            (when padding
+;              (dotimes [offset padding]
+;                (put! \space (+ x offset) y -text))))
+;          (print-from! [x emission chars]
+;            (reduce-idx
+;              (fn [x' _ input]
+;                (put! input x' y emission)) x nil chars))
+;          (print! [x emission chars]
+;            (print-from! x emission chars)
+;            (+ x (count chars)))
+;          (print-sub! [x emission chars]
+;            (let [x' (+ x (count chars))]
+;              (cond
+;                (and (<= x xs) (>= x' xe)) (->> chars
+;                                                (drop (- xs x)) ;; subvecs
+;                                                (take (- xe xs)) ;; subvecs
+;                                                (print-from! xs emission))
+;                (and (<= x xs) (> x' xs))  (->> chars
+;                                                (drop (- xs x)) ;; subvecs
+;                                                (print-from! xs emission))
+;                (>= x' xe)                 (->> chars
+;                                                (take (- xe x)) ;; subvecs
+;                                                (print-from! x emission))
+;                (> x' xs)                  (->> chars
+;                                                (print-from! x emission))
+;                :else                      nil)
+;              x'))]
+;    (-> (if (and xs xe) print-sub! print!)
+;        (fold 0 line)
+;        (pad!))))
+
+;(s/defn print-line!
+;        [{line     :line
+;          y        :at
+;          xs       :start
+;          xe       :end
+;          padding  :padding
+;          terminal :terminal
+;          cs       :scheme
+;          styles   :styles} :- PrintInstruction]
+;        (let [put!  (fn [char x y emission]
+;                      (let [fg (get cs (coloured-element emission))
+;                            bg (get cs backgrounds)]
+;                        (t/put! terminal char x y fg bg styles)))
+;              pad!  (fn [x]
+;                      (when padding
+;                        (dotimes [offset padding]
+;                          (put! \space (+ x offset) y -text))))
+;              put-from! (fn [x emission chars]
+;                          (reduce-idx
+;                            (fn [x' _ input]
+;                              (put! input x' y emission)) x nil chars))
+;              print!    (fn [x emission chars]
+;                          (let [x' (+ x (count chars))]
+;                            (cond
+;                              (and (<= x xs) (>= x' xe)) (->> chars
+;                                                              (drop (- xs x)) ;; subvecs
+;                                                              (take (- xe xs)) ;; subvecs
+;                                                              (put-from! xs emission))
+;                              (and (<= x xs) (> x' xs))  (->> chars
+;                                                              (drop (- xs x)) ;; subvecs
+;                                                              (put-from! xs emission))
+;                              (>= x' xe)                 (->> chars
+;                                                              (take (- xe x)) ;; subvecs
+;                                                              (put-from! x emission))
+;                              (> x' xs)                  (->> chars
+;                                                              (put-from! x emission)) ;; does this also catch the total case?
+;                              :else                      nil)
+;                            x'))]
+;          (->> line (fold print! 0) (pad!))))
 
 (s/defn prioritise :- [c/Highlight]
-  [highlights :- c/Highlights]
-  (->> highlights (sort-by (comp highlight-priority key)) (map second)))
+        [highlights :- c/Highlights]
+        (->> highlights (sort-by (comp highlight-priority key)) (map second)))
 
 (s/defn prioritised :- [c/Highlight]
-  [ctx :- Context, pattern :- HighlightPattern]
-  (let [current     (:current pattern)
-        former      (:former pattern)
-        preview-ov  (-> ctx (c/preview-hud) (h/view-offset))
-        previous-ov (-> ctx (c/previous-hud) (h/view-offset))]
-    (if (= preview-ov previous-ov)
-      ;; additive-diff nils highlights that don't have a diff
-      (prioritise (merge-culling additive-diff current former))
-      (prioritise current))))
+        [ctx :- Context, pattern :- HighlightPattern]
+        (let [current     (:current pattern)
+              former      (:former pattern)
+              preview-ov  (-> ctx (c/preview-hud) (h/view-offset))
+              previous-ov (-> ctx (c/previous-hud) (h/view-offset))]
+          (if (= preview-ov previous-ov)
+            ;; additive-diff nils highlights that don't have a diff
+            (prioritise (merge-culling additive-diff current former))
+            (prioritise current))))
+
+(s/defn put!
+  [terminal  :- Terminal
+   character :- Character
+   x         :- s/Int
+   y         :- s/Int
+   emission  :- ColouredElement
+   scheme    :- UserHighlighting
+   styles    :- [Style]]
+  (let [fg (get scheme (coloured-element emission))
+        bg (get scheme backgrounds)]
+    (t/put! terminal character x y fg bg styles)))
+
+(s/defn print-line!
+  [terminal :- Terminal
+   line     :- i/Line
+   y        :- s/Int
+   padding  :- s/Int
+   scheme   :- UserHighlighting
+   styles   :- [Style]]
+  (let [pad!   (fn [x]
+                 (dotimes [offset padding]
+                   (put! terminal \space (+ x offset) y -text scheme styles)))
+        print! (fn [x emission chars]
+                 (reduce-idx
+                   (fn [x' _ character]
+                     (put! terminal character x' y emission scheme styles)) x nil chars)
+                 (+ x (count chars)))]
+    (->> line (fold print! 0) (pad!))))
+
+(s/defn print-highlight!
+  [terminal  :- Terminal
+   hud       :- h/Hud
+   highlight :- c/Highlight]
+  (let [selection (->> highlight (:region) (h/clip-selection hud))
+        scheme    (->> highlight (:scheme))
+        styles    (->> highlight (:styles))
+        seeker    (h/text hud)
+        [xs ys]   (:start selection)
+        [xe ye]   (:end selection)]
+    (doseq [y (range ys (inc ye))]
+      (let [line  (i/line seeker [0 y])
+            xs    (if (= y ys) xs 0)
+            xe    (if (= y ye) xe (count line))
+            y'    (h/project-y hud y)]
+        (doseq [x' (range xs xe)]
+          (put! terminal (nth line x') x' y' -text scheme styles))))))
 
 (s/defn highlight!
-  "This thing should not do any selection extraction
-   It should purely just do lookups and recursion over indexes"
   [terminal :- Terminal,
    ctx      :- Context,
    pattern  :- HighlightPattern]
   (let [hud        (:hud pattern)
         highlights (prioritised ctx pattern)]
-    (doseq [{region :region
-             scheme :scheme
-             styles :styles} highlights]
-      (let [selection (h/clip-selection hud region)
-            seeker    (h/text hud)
-            [xs ys]   (:start selection)]
-        (->> selection
-             (i/extract-for seeker)
-             (:lines)
-             (reduce-idx
-               (fn [y x line]
-                 (print-line! {:at         (h/project-y hud y)
-                               :line       (i/line seeker [x y])
-                               :terminal   terminal
-                               :scheme     scheme
-                               :styles     styles
-                               :sub-region [x (-> line (count) (+ x))]})
-                 0) ys xs))))))
+    (doseq [highlight highlights]
+      (print-highlight! terminal hud highlight))))
 
+;; FIXME: Now that I've divided simple rendering from highlighting, cleaning highlights doesn't print the normal highlighting anymore
 (s/defn clean-highlights!
   [terminal :- Terminal,
    ctx      :- Context]
@@ -197,15 +277,10 @@
         scheme   (-> ctx (c/configuration) (:syntax) (:standard))
         limit    (max (count now) (count then))]
     (dotimes [y limit]
-      (let [a (nth now y nil)
-            b (nth then y nil)]
-        (print-line!
-          {:at       y
-           :line     a
-           :padding  (padding a b)
-           :terminal terminal
-           :scheme   scheme
-           :styles  []})))))
+      (let [a   (nth now y nil)
+            b   (nth then y nil)
+            pad (padding a b)]
+        (print-line! terminal a y pad scheme [])))))
 
 (s/defn render-diff!
   [terminal :- Terminal
@@ -220,15 +295,11 @@
     (if (not= (h/view-offset preview) (h/view-offset previous))
       (render-total! terminal ctx)
       (dotimes [y limit]
-        (let [a (nth now y nil)
-              b (nth then y nil)]
+        (let [a   (nth now y nil)
+              b   (nth then y nil)
+              pad (padding a b)]
           (when (not= a b)
-            (print-line! {:at       y
-                          :line     a
-                          :padding  (padding a b)
-                          :terminal terminal
-                          :scheme   scheme
-                          :styles   []})))))))
+            (print-line! terminal a y pad scheme [])))))))
 
 (s/defn render-nothing!
   [terminal :- Terminal,
