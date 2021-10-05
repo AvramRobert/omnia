de248007012fdd6d4ff516e4339f444a880f85da
Robert M. Avram <am11.robert@yahoo.com>
2021-10-05 23:18:53 +0200

    Test executable bat and add windows to releases

--- release/templates/executable.bat
+++ release/templates/executable.bat
@@ -1,4 +1,4 @@
-echo off
+@echo off
 
 echo "Starting Omnia %%VERSION%%.."
 
--- src/omnia/release/core.clj
+++ src/omnia/release/core.clj
@@ -1,35 +1,46 @@
 (ns omnia.release.core
   (:require [clojure.java.shell :as shell]
+            [schema.core :as s]
             [halfling.task :as t]
-            [clojure.string :as s]
-            [omnia.config.core :refer [default-user-config]]
+            [clojure.string :as string]
+            [omnia.config.core :refer [UserConfig default-user-config]]
             [omnia.util.misc :refer [omnia-version]]))
 
-(def release-configuration
+(def OS (s/enum :linux :macOS :windows))
+
+(def Executable (s/enum :sh :bat))
+
+(def ReleaseConfig
+  {:template      s/Str
+   :file-type     Executable
+   :configuration UserConfig})
+
+(def Releases {OS ReleaseConfig})
+
+(s/def releases :- Releases
   {:linux   {:template      "release/templates/executable.sh"
-             :file-type     "sh"
+             :file-type     :sh
              :configuration default-user-config}
-   :macOS   {:template      "release/templates/executable.sh"
-             :file-type     "sh"
+   :windows {:template      "release/templates/executable.bat"
+             :file-type     "bat"
              :configuration default-user-config}
-   ;:windows {:template      "release/templates/executable.bat"
-   ;          :file-type     "bat"
+
+   ;:macOS   {:template      "release/templates/executable.sh"
+   ;          :file-type     :sh
    ;          :configuration default-user-config}
-   ;
    })
 
 (defn- sh [& args]
-  (-> (t/task (apply shell/sh args))
-      (t/then (fn [{:keys [out exit err]}]
-                (if (neg? exit)
-                  (t/failure err)
-                  (println out))))))
+  (let [{:keys [out exit err]} (apply shell/sh args)]
+    (if (neg? exit)
+      (throw (Exception. err))
+      (println out))))
 
-(defn- make-executable [path jar-filename version]
+(defn- make-executable [path file-name version]
   (-> path
       (slurp)
-      (s/replace "%%FILENAME%%" jar-filename)
-      (s/replace "%%VERSION%%" version)))
+      (string/replace "%%FILENAME%%" file-name)
+      (string/replace "%%VERSION%%" version)))
 
 (defn mkdir [directory]
   (sh "mkdir" directory))
@@ -46,35 +57,36 @@
 (defn lein [command]
   (sh "lein" command))
 
+
 (defn release-for [os]
-  (t/do-tasks
-    [system      (name os)
-     version     (omnia-version)
-     name        "omnia"
-     release     (get release-configuration os)
-     _           (println "Releasing for: " system)
+  (let [system      (name os)
+        version     (omnia-version)
+        file-name   "omnia"
+        release     (get releases os)
+        _           (println "Releasing for: " system)
+        target-ext  (-> release (:file-type) (name))
+        target-jar  (str file-name ".jar")
+        target-conf (str file-name ".edn")
+        target-exec (str file-name "." target-ext)
+        target-font "default_font.otf"
+        target-dir  (format "%s-%s-%s" file-name version system)
 
-     target-jar  (str name ".jar")
-     target-conf (str name ".edn")
-     target-exec (str name "." (:file-type release))
-     target-font "default_font.otf"
-     target-dir  (format "%s-%s-%s" name version system)
-
-     jar-file    (format "target/uberjar/%s-%s-standalone.jar" name version)
-     executable  (-> release (:template) (make-executable name version))
-     config      (-> release-configuration (get os) (:configuration))
-     font-file   "release/Hasklig-Regular.otf"
-     _           (mkdir (str "./" target-dir))
-     _           (println "Creating release files..")
-     _           (cp jar-file (str target-dir "/" target-jar))
-     _           (cp font-file (str target-dir "/" target-font))
-     _           (spit (str target-dir "/" target-conf) config)
-     _           (spit (str target-dir "/" target-exec) executable)
-     _           (println "Creating archive..")
-     _           (zip-dir (str target-dir ".zip") target-dir)
-     _           (println "Removing directory..")
-     _           (rm-dir target-dir)
-     _           (println "Done!")]))
+        jar-file    (format "target/uberjar/%s-%s-standalone.jar" file-name version)
+        executable  (-> release (:template) (make-executable file-name version))
+        config      (-> release (:configuration))
+        font-file   "release/Hasklig-Regular.otf"
+        _           (mkdir (str "./" target-dir))
+        _           (println "Creating release files..")
+        _           (cp jar-file (str target-dir "/" target-jar))
+        _           (cp font-file (str target-dir "/" target-font))
+        _           (spit (str target-dir "/" target-conf) config)
+        _           (spit (str target-dir "/" target-exec) executable)
+        _           (println "Creating archive..")
+        _           (zip-dir (str target-dir ".zip") target-dir)
+        _           (println "Removing directory..")
+        _           (rm-dir target-dir)
+        _           (rm-dir "target")
+        _           (println "Done!")]))
 
 (def release-task
   (t/do-tasks
@@ -84,7 +96,7 @@
      _ (println "Creating jar")
      _ (lein "uberjar")
      _ (println "--------------")
-     _ (->> release-configuration (keys) (mapv release-for) (t/sequenced))
+     _ (->> releases (keys) (run! release-for))
      :recover #(do (.printStackTrace %) (System/exit -1))]))
 
 (defn release []
