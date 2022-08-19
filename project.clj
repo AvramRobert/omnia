55c07b091d16a416e44d428083ef3282fb46ad44
Robert Avram <robert.avram@rewe-digital.com>
2022-08-19 12:55:35 +0200

    Update deps

--- project.clj
+++ project.clj
@@ -3,21 +3,21 @@
   :url "https://github.com/AvramRobert/omnia"
   :license {:name "Apache-2.0 License"
             :url  "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  [com.googlecode.lanterna/lanterna "3.1.1"]
                  [org.clojure/core.match "1.0.0"]
-                 [fipp "0.6.24"]
-                 [prismatic/schema "1.2.0"]
+                 [fipp "0.6.26"]
+                 [prismatic/schema "1.4.0"]
                  [halfling "1.3.1"]
-                 [instaparse "1.4.10"]
-                 [cider/cider-nrepl "0.28.1"]
+                 [instaparse "1.4.12"]
+                 [cider/cider-nrepl "0.28.5"]
                  [clj-commons/pomegranate "1.2.1"]
                  [org.tcrawley/dynapath "1.1.0"]
-                 [org.slf4j/slf4j-simple "1.7.30"]
+                 [org.slf4j/slf4j-simple "1.7.36"]
                  [nrepl "0.9.0"]]
   :main ^:skip-aot omnia.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[criterium "0.4.4"]
-                                  [org.clojure/java.data "1.0.95"]]}}
+             :dev     {:dependencies [[criterium "0.4.6"]
+                                      [org.clojure/java.data "1.0.95"]]}}
   :aliases  {"release" ["run" "-m" "omnia.release.core/release!"]})
